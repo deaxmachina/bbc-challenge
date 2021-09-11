9af14f6f815bbd21a42b7707e3387da6d26a423c
deaxmachina <d.y.bankova@gmail.com>
2021-09-11 20:06:53 +0900

    added scroll to menu and organised main file

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 
-import { testGraph } from "./modules/test.js"
 import { getCityData } from "./modules/dataPrep.js"
 import { canvas, initParticles, animateParticles } from "./modules/aqiParticles.js"
 import { cigsViz } from "./modules/cigsViz.js"
-import { selectMenu } from "./modules/selectMenu.js"
+//import { selectMenu } from "./modules/selectMenu.js"
 
+// DOM selectors 
+const sigsContainer = d3.select("#cigarette")
+const cityInput = d3.select('#cityInput')
+const cityList = d3.select('#cityList')
+const aqiValue = d3.select('.aqi-value')
+
+// Initialise data 
 let rawData;
 let cityData; 
-// Initialise data 
 let selectedCity = ''
 let numParticles = 0
 let numCigs
@@ -36,56 +41,42 @@ if (window.innerWidth < 600) {
 
 // When a city is selected, update the selectedCity and nums for aqi and 
 // and draw the graph with updated values
-const citiesDropdown = document.getElementById('cities-select')
-citiesDropdown.addEventListener('change', e => {
-  selectedCity = e.target.value;
-  numParticles = cityData.filter(city => city.name === selectedCity)[0].aqi
-  const aqiValue = document.querySelector('.aqi-value')
-  aqiValue.innerHTML = numParticles
-  graph(selectedCity)
-})
+// const citiesDropdown = document.getElementById('cities-select')
+// citiesDropdown.addEventListener('change', e => {
+//   selectedCity = e.target.value;
+//   numParticles = cityData.filter(city => city.name === selectedCity)[0].aqi
+//   const aqiValue = document.querySelector('.aqi-value')
+//   aqiValue.innerHTML = numParticles
+//   graph(selectedCity)
+// })
 
-const sigsContainer = d3.select("#cigarette");
-const input = document.getElementById('cityInput');
-const ul = document.getElementById("cityList");
-//const li = ul.getElementsByTagName('li');
-//let li
-
-
-async function graph(selectedCity) {
-  // 1. Load in the raw data and extract useful data in array format
-  rawData = await d3.json('english.json')
-  cityData = getCityData(rawData)
-
-  // 2. Add options for each city from the data 
-  const selectMenuContainer = d3.select('#cities-select')
-  selectMenuContainer.call(selectMenu, cityData)
-
-  const cityInput = d3.select('#cityInput')
-  const cityList = d3.select('#cityList')
+const getCitiesMenu = (cityData, cityInput) => {
   const listCities = cityList.selectAll('li')
     .data(cityData)
     .join('li')
     .html(d => d.name)
     .classed('city-option', true)
-    .style('display', 'none')
+    .style('display', 'none') // Don't display list of cities unless searched
     .on('click', function(e) {
-      input.value = e.target.innerHTML
-      listCities.style('display', 'none')
+      selectedCity = e.target.innerHTML
+      cityInput.property('value', selectedCity) // Search field input updates to city
+      listCities.style('display', 'none') // Hide list of cities again
+      numParticles = cityData.filter(city => city.name === selectedCity)[0].aqi // Update num particles for particle graph
+      // aqiValue.html(numParticles)
+      graph(selectedCity) // Run the graph with the selected city
     })
 
-
-  cityInput.on('keyup', function(e, datum) {
-    let filter = this.value.toUpperCase();
+  // Search functionality - display only cities that contain keyboard input in search field
+  cityInput.on('keyup', function() {
+    let filter = this.value.toUpperCase(); // Current input
     // If input is empty, hide options
     if (filter.length === 0) {
       listCities.style('display', 'none')
     } else {
       // Loop through all list items, and hide those who don't match the search query
       for (let i = 0; i < listCities.nodes().length; i++) {
-        const a = listCities.nodes()[i];
-        console.log(a)
-        const txtValue = a.textContent || a.innerText;
+        const city = listCities.nodes()[i];
+        const txtValue = city.textContent || city.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
           listCities.nodes()[i].style.display = "";
         } else {
@@ -94,16 +85,27 @@ async function graph(selectedCity) {
       }
     }
   })
-  
-  // 3. Call the graphs on the container with the data
-  //svg.call(testGraph, cityData)
-  sigsContainer.call(cigsViz, selectedCity, cityData, numCigs, sigsDims)
-
-  // 4. Draw particles 
-  initParticles(numParticles)
-  //animateParticles() -- make sure this happens after graph and it is not ever called again
 }
 
+
+async function graph(selectedCity) {
+  // 1. Load in the raw data and extract useful data in array format
+  rawData = await d3.json('english.json')
+  cityData = getCityData(rawData)
+
+  // 2. Initialise the menu of cities (list) behind the search functionality
+  // const selectMenuContainer = d3.select('#cities-select')
+  // selectMenuContainer.call(selectMenu, cityData)
+  getCitiesMenu(cityData, cityInput)
+
+  // 3. Call the cigarettes viz
+  sigsContainer.call(cigsViz, selectedCity, cityData, numCigs, sigsDims)
+
+  // 4. Initialise the particles for the PM2.5 particle graph
+  initParticles(numParticles)
+}
+
+// Run the graph + animate the particles
 graph(selectedCity)
 animateParticles()
 
--- modules/selectMenu.js
+++ modules/selectMenu.js
@@ -1,52 +1,91 @@
-const selectMenu = (container, cityData) => {
-  container.selectAll('option')
-    .data(cityData)
-    .join('option')
-      .attr('value', d => d.name)
-      .html(d => d.name)
-}
-
-
-// const input = document.getElementById('cityInput');
-// const ul = document.getElementById("cityList");
-// const li = ul.getElementsByTagName('li');
-
-const hideDropdownList = (li) => {
-  for (const liEl of li) {
-    liEl.style.display = "none";
-  }
-}
-
-// hideDropdownList()
-
-// for (const liEl of li) {
-//   liEl.addEventListener('click', e => {
-//     input.value = e.target.innerHTML
-//     hideDropdownList()
-//   })
+// const selectMenu = (container, cityData) => {
+//   container.selectAll('option')
+//     .data(cityData)
+//     .join('option')
+//       .attr('value', d => d.name)
+//       .html(d => d.name)
 // }
 
-// input.addEventListener('keyup', e => searchMatches(e))
 
-function searchMatches(li, input) {
-  let filter = input.value.toUpperCase();
-  // If input is empty, hide options
-  if (filter.length === 0) {
-    hideDropdownList(li)
-  } else {
-    // Loop through all list items, and hide those who don't match the search query
-    for (let i = 0; i < li.length; i++) {
-      const a = li[i];
-      const txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
+// // const input = document.getElementById('cityInput');
+// // const ul = document.getElementById("cityList");
+// // const li = ul.getElementsByTagName('li');
+
+// const hideDropdownList = (li) => {
+//   for (const liEl of li) {
+//     liEl.style.display = "none";
+//   }
+// }
+
+// // hideDropdownList()
+
+// // for (const liEl of li) {
+// //   liEl.addEventListener('click', e => {
+// //     input.value = e.target.innerHTML
+// //     hideDropdownList()
+// //   })
+// // }
+
+// // input.addEventListener('keyup', e => searchMatches(e))
+
+// function searchMatches(li, input) {
+//   let filter = input.value.toUpperCase();
+//   // If input is empty, hide options
+//   if (filter.length === 0) {
+//     hideDropdownList(li)
+//   } else {
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (let i = 0; i < li.length; i++) {
+//       const a = li[i];
+//       const txtValue = a.textContent || a.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         li[i].style.display = "";
+//       } else {
+//         li[i].style.display = "none";
+//       }
+//     }
+//   }
+// }
+
+const getCitiesMenu = (cityData, cityInput, cityList, selectedCity, numParticles, graph) => {
+  const listCities = cityList.selectAll('li')
+    .data(cityData)
+    .join('li')
+    .html(d => d.name)
+    .classed('city-option', true)
+    .style('display', 'none')
+    .on('click', function(e) {
+      selectedCity = e.target.innerHTML
+      cityInput.property('value', selectedCity)
+      listCities.style('display', 'none')
+
+      numParticles = cityData.filter(city => city.name === selectedCity)[0].aqi
+      const aqiValue = document.querySelector('.aqi-value')
+      aqiValue.innerHTML = numParticles
+      graph(selectedCity)
+    })
+
+  cityInput.on('keyup', function() {
+    let filter = this.value.toUpperCase(); // Current input
+    // If input is empty, hide options
+    if (filter.length === 0) {
+      listCities.style('display', 'none')
+    } else {
+      // Loop through all list items, and hide those who don't match the search query
+      for (let i = 0; i < listCities.nodes().length; i++) {
+        const city = listCities.nodes()[i];
+        const txtValue = city.textContent || city.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          listCities.nodes()[i].style.display = "";
+        } else {
+          listCities.nodes()[i].style.display = "none";
+        }
       }
     }
-  }
+  })
 }
 
 
 
-export { selectMenu, hideDropdownList, searchMatches }
+
+export { getCitiesMenu }
